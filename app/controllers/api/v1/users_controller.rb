73a8dbdbde8f41c1834134f76c2f67b94f0aa6e2
BenMiriello <benmiriello@gmail.com>
2020-07-04 10:25:38 -0400

    add auth for create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,12 @@
 module Api::V1
   class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+    
     def create
-      @user = User.create(user_params)
-      if @user.valid?
-        render json: {user: UserSerializer.new(@user)}, status: :created
+      user = User.create(user_params)
+      if user.valid?
+        token = encode_token(user_id: user.id)
+        render json: {user: UserSerializer.new(user), jwt: token}, status: :created
       else
         render json: {error: 'failed to create user.'}, status: :not_acceptable
       end
