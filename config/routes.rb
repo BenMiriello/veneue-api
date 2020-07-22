7a8ac4b43060d897c02533d104345b2145bd5b0b
BenMiriello <benmiriello@gmail.com>
2020-07-21 23:50:56 -0400

    Add routes for video room

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  constraints subdomain: 'api' do
-    scope module: 'api' do
+  constraints subdomain: "api" do
+    scope module: "api" do
       namespace :v1 do
-        post '/signup', to: "users#create"
-        patch '/change_email', to: "users#change_email"
-        patch '/change_password', to: "users#change_password"
-        patch '/change_name', to: "users#change_name"
-        delete '/delete_account', to: "users#destroy"
+        post "/signup", to: "users#create"
+        patch "/change_email", to: "users#change_email"
+        patch "/change_password", to: "users#change_password"
+        patch "/change_name", to: "users#change_name"
+        delete "/delete_account", to: "users#destroy"
 
-        post '/login', to: "sessions#create"
-        get '/check_logged_in', to: "sessions#check_logged_in"
-        delete '/logout', to: "sessions#logout"
+        post "/login", to: "sessions#create"
+        get "/check_logged_in", to: "sessions#check_logged_in"
+        delete "/logout", to: "sessions#logout"
+
+        resources :video_rooms, only: [:create, :update, :destroy]
       end
     end
   end
